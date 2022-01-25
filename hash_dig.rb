aec100d79b077e50cef9da50195bf810ad82b70d
trevor-mill <trevor.million@chicagobooth.edu>
2022-01-25 04:58:19 +0000

    finished dig and person

--- hash_dig.rb
+++ hash_dig.rb
@@ -1,13 +1,20 @@
-# Write a program that uses the sample_hash variable and prints the value of key "history"
+# # Write a program that uses the sample_hash variable and prints the value of key "history"
 
+# sample_hash = {
+#    :class => { 
+#       :student => { 
+#          :name => "Mike",
+#          "marks" => { 
+#             "physics" => 70,
+#             "history" => 80
+#          }
+#       }
+#    }
+# }
 sample_hash = {
-   :class => { 
-      :student => { 
-         :name => "Mike",
-         "marks" => { 
-            "physics" => 70,
-            "history" => 80
-         }
-      }
-   }
+   :student => "Mike",
+   :physics => 70,
+   :history => 80
 }
+
+p sample_hash.fetch(:history)
--- hash_person.rb
+++ hash_person.rb
@@ -1,2 +1,9 @@
 # Ask for a name, age, and occupation; store the values in a Hash; display the Hash.
 p "Enter a name, age, and occupation separated by spaces:"
+input = gets.chomp.split
+# p input
+output = {}
+output.store(:name, input[0])
+output.store(:age, input[1].to_i)
+output.store(:occupation, input[2])
+p output
